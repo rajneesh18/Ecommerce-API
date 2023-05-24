0364407e3f677e14ce0648e1f4d6fd5f59f5cdb7
rajneesh18 <44999300+rajneesh18@users.noreply.github.com>
2023-05-24 17:23:19 +0530

    2 files modified, 5 added

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-// run `node index.js` in the terminal
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const PORT = process.env.PORT || 4000;
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const app = express();
+const DB = require('./config/db');
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+const authRoute = require('./routes/auth');
+const { notFound, errorHandler } = require('./middleware/errorHandling');
+
+app.use('/api/user', authRoute);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const server = () => { 
+  app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`); 
+  });
+}
+DB(server);
